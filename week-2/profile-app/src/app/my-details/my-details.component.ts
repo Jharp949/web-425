4030f795eb8ebdfefb613eb8457b946b155e40fa
James Harper <jharp949@gmail.com>
2023-10-29 21:28:17 -0700

    created exercise 2.4

---
+++ week-2/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "TypeScript", "#2023", "CodingWithAngular", "#ngOmaha"
+  ];
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteColor;
+    this.favoriteColor = favoriteColor;
+  }
+
+  toString() {
+    console.log(`\n Full name: ${this.fullName}\n Favorite food: ${this.favoriteFood}\n Favorite color: ${this.favoriteColor}`)
+  }
+
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  myProfile: Person;
+  constructor() {
+    this.myProfile = new Person("James Harper", "Fried Chicken", "Purple");
+    this.myProfile.toString();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
