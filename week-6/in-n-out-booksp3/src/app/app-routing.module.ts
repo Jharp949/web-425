8b052a5cd3c4190a3d0d3bd43380951e8cca3cad
James Harper <jharp949@gmail.com>
2023-12-01 20:41:09 -0800

    Copied booksp3

---
+++ week-6/in-n-out-booksp3/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+/**
+ * Title: app-routing.module.ts
+ * Author: James Harper
+ * Date: 19 November 2023
+ * Description: Routing file
+ */
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
